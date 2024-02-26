fcbe8f6f680862d088b5cffd6d2e20bcf155bd0b
Aditya Sher <adityasher2000@gmail.com>
2024-02-26 14:28:04 +0530

    Add /stream pathname for streaming poem data
    Modify HTML code for fetching streamed poem for uncached poems

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ import { Ai } from '@cloudflare/ai';
 
 export default {
 	async fetch(request, env, ctx) {
-		return await handleResponse(request,env)
+		return await handleResponse(request, env)
 	},
 };
 
-async function handleResponse(request,env) {
+async function handleResponse(request, env) {
 	// extract the required data from the cloudflare request object
 	const continent = request.cf.continent
 	const country = request.cf.country
@@ -26,26 +26,34 @@ async function handleResponse(request,env) {
 	//create the poem using Cloudflare AI
 	const prompt = `You are now a poet, write a poem on the following region Continent: ${continent}, Country: ${country}, Region: ${region} `
 
+	const url = new URL(request.url);
+	const path = url.pathname;
+
+	if (path == "/stream") {
+		const poemStream = await createPoem(prompt, env)
+
+		return new Response(
+			poemStream,
+			{ headers: { "content-type": "text/event-stream" } }
+		);
+	}
+
 	// create a hash and check if poem already exists
-	const currentHash = await createHash(continent,country,region)
+	const currentHash = await createHash(continent, country, region)
 
 	const cachedPoem = await env.POEMS.get(currentHash)
 
 	let poem = cachedPoem
-
-	if(!cachedPoem){
-		poem = await createPoem(prompt,env)
-		await env.POEMS.put(currentHash,poem)
-	}
-
-	poem = poem.replace(/\n/g, '<br>');
-
+	let html_content = ""
 	const html_style = "body{padding:6em; font-family: sans-serif;} h1{color:#f6821f;}";
 
-	let html_content = ""
-	html_content += "<p>" +  poem + "</p>";
-	
-	let html = `<!DOCTYPE html>
+	// for already generated poem, show it directly
+	if (cachedPoem) {
+		poem = poem.replace(/\n/g, '<br>');
+		html_content += "<p>" + poem + "</p>";
+
+
+		let html = `<!DOCTYPE html>
 	<head>
 	  <title> Here's a poem for you </title>
 	  <style> ${html_style} </style>
@@ -55,6 +63,39 @@ async function handleResponse(request,env) {
 	  ${html_content}
 	</body>`
 
+
+	}
+
+	// for generating a new poem use the /stream path
+	else {
+		html_content = `<div id="poem"></div>
+    <script>
+        const poemDiv = document.getElementById('poem');
+        const evtSource = new EventSource('/stream');
+        evtSource.onmessage = function(event) {
+			if (event.data == "[DONE]") {
+				evtSource.close();
+				return;
+			}
+			const eventData = JSON.parse(event.data);
+			const poemLine = eventData.response.replace(/\\n/g, '<br>');
+            poemDiv.innerHTML +=  poemLine
+        };
+    </script>`;
+
+	}
+
+
+	let html = `<!DOCTYPE html>
+    <head>
+        <title>Here's a poem for you</title>
+        <style>${html_style}</style>
+    </head>
+    <body>
+        <h1>Here's a poem for you</h1>
+        ${html_content}
+    </body>`;
+
 	return new Response(html, {
 		headers: {
 			"content-type": "text/html;charset=UTF-8",
@@ -62,33 +103,33 @@ async function handleResponse(request,env) {
 	});
 }
 
-async function createPoem(input,env) {
+async function createPoem(input, env) {
 	const ai = new Ai(env.AI)
-	
+
 	const output = await ai.run('@cf/mistral/mistral-7b-instruct-v0.1', {
 		prompt: input,
-		max_tokens: 1000
+		max_tokens: 1000,
+		stream: true
 	})
 
-	console.log(JSON.stringify(output))
-	return output.response
+	return output
 }
 
 // Create a hash by combining the 3 strings and multiplying by 31 (2^5)
-async function createHash(continent,country,region){
+async function createHash(continent, country, region) {
 	const string = continent + country + region
 
 	let hash = 0;
 
 	if (string.length === 0) {
-        return hash;
-    }
-    for (let i = 0; i < string.length; i++) {
-        let char = string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash; // Convert to 32bit integer
-    }
-    return hash;
+		return hash;
+	}
+	for (let i = 0; i < string.length; i++) {
+		let char = string.charCodeAt(i);
+		hash = ((hash << 5) - hash) + char;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
 }
 
 
