42edf0c250e13055c0e2791576f5330adc5cfc8f
Aditya Sher <adityasher2000@gmail.com>
2024-02-26 11:44:24 +0530

    add function for creating hash

--- src/index.js
+++ src/index.js
@@ -62,3 +62,21 @@ async function createPoem(input,env) {
 	console.log(JSON.stringify(output))
 	return output.response
 }
+
+async function createHash(continent,country,region){
+	const string = continent + country + region
+
+	let hash = 0;
+
+	if (string.length === 0) {
+        return hash;
+    }
+    for (let i = 0; i < string.length; i++) {
+        let char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+
